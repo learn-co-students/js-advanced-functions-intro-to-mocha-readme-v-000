102db8bad8765abd2fd6b70b692bdc9154c3d3fc
Mollie <mollie.stein@gmail.com>
2017-10-03 20:01:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
+
+
