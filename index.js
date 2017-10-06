836f5ef2d4add4df0d6a6a526df9f72b9a5e4e84
ruuuh20 <pkim2320@gmail.com>
2017-10-06 16:37:16 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+// function favoriteIceCream(flavor) {
+//   debugger
+// }
+
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(caps) {
+  return caps.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.floor(number)
+}
