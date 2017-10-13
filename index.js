58c0313fa039e38c9ef6a77bf967502e406da18d
Luisa Scavo <luisascavo@gmail.com>
2017-10-12 22:25:10 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return 'Hey!';
+}
+
+function sayHeyFriend(friend){
+  return `Hey, ${friend}!`;
+}
+
+function favoriteIceCream(flav){
+  return `I love ${flav}`;
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
