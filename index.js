b6f51540e13d6118588234ae7331574a8c82bbf0
Michael Burak <michaelrburak@gmail.com>
2017-10-25 16:13:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(x) {
+  return `I love ${x}`
+}
+
+function shouting(x) {
+return   x.toUpperCase()
+}
+
+function roundDown(x) {
+  return Math.floor(x)
+}
