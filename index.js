4ee0323e92deeae8e25f8e1977c3cb2e3c38ff82
alecgalba <alecgalba@gmail.com>
2017-10-30 18:54:05 -0400

    Coffee

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(nombre) {
+  return `Hey, ${nombre}!`;
+}
+
+function favoriteIceCream(ice_cream) {
+  return `I love ${ice_cream}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
