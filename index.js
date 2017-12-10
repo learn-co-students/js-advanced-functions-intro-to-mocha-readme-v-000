cd76dc2806745a946f6b44073b2e3c66ef4eaac5
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-12-10 18:54:03 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+	return true
+}
+
+function sayHey() {
+	return "Hey!"
+}
+
+function sayHeyFriend(NAME) {
+	return "Hey, " + NAME + "!"
+}
+
+function favoriteIceCream(NAME_OF_ICECREAM) {
+	return "I love " + NAME_OF_ICECREAM
+}
+
+function shouting(string) {
+	return string.toUpperCase()
+}
+
+function roundDown(num) {
+	return Math.floor(num)
+}
