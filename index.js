88030457dc01b4257551f43845fdda4ccb8a10ba
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-15 09:50:31 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+	return true;
+}
+
+function sayHey() {
+	return "Hey!";
+}
+
+function sayHeyFriend(name) {
+	return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(iceCream) {
+	debugger;
+	return `I love ${iceCream}`;
+}
+
+function shouting(msg) {
+	return msg.toUpperCase();
+}
+
+function roundDown(num) {
+	return Math.floor(num);
+}
