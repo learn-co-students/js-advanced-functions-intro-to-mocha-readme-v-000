83824bd9b369424fdef50d6f7e5a3ccc8b0b7b8e
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-20 01:16:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!";
+}
+
+function sayHeyFriend(name){
+
+}
+
+function favoriteIceCream(){
+
+}
+
+function shouting(){
+
+}
+
+function roundDown(){
+
+}
