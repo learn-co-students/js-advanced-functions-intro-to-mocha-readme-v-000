ea9958c0776b05e6b7e0a4ad7303083abb49ab87
kyleblee <hello.kylelee@gmail.com>
2018-01-09 19:19:35 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`;
+}
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
