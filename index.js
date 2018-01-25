007dd01fa9a302a57ab5762cbf5331b9e9bf551d
James Randolph <james.randolph2016@gmail.com>
2018-01-25 01:44:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend() {
+  var name = "Kristin";
+  return 'Hey, ' + name + '!'
+}
+
+function favoriteIceCream() {
+  var NAME_OF_ICECREAM = "mint chocolate chip"
+  return 'I love ' + NAME_OF_ICECREAM
+}
+
+function shouting() {
+ var str = "I love javascript soooooo much"
+ var res = str.toUpperCase();
+ return res
+}
+
+function roundDown() {
+  var str = 5.4
+  var res = Math.floor(str)
+  return res
+}
