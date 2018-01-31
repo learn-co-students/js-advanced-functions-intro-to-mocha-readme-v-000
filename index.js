3b5dbb745802d0ca0ccfaeae022af28e4ff5eb68
CodeDad <github email address>
2018-01-30 21:54:24 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theTruth() {
+	return true;
+};
+
+function sayHey() {
+	return "Hey!";
+};
+
+function theTruth() {
+	return true;
+};
+
+function sayHeyFriend(friend){
+	return "Hey, "+ friend + "!";
+};
+
+function favoriteIceCream(iceCream) {
+	return 'I love ' + iceCream;
+};
+
+function shouting(stringCap) {
+	return stringCap.toUpperCase();
+};
+
+function roundDown(number) {
+	return Math.floor(number);
+};
