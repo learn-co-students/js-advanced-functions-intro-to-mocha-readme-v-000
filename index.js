971646ffd99e76991f947eec34a97800bac0a2e6
ackerm44 <ackerm44@gmail.com>
2018-02-02 11:33:12 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(ice_cream) {
+  return `I love ${ice_cream}`
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
