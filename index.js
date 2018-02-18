1cd644405efc24a22952eb08e6d72500ca51cf92
Oski <github email address>
2018-02-18 08:26:53 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var theTruth = () => true;
+
+var sayHey = () => 'Hey!'
+
+var sayHeyFriend = (name) => `Hey, ${name}!`
+
+var favoriteIceCream = (ice) => `I love ${ice}`
+
+var shouting = (s) => s.toUpperCase();
+
+var roundDown = (n) => Math.floor(n);
