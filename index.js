2bfccaf084359675f92a82ce3c17e367009bc161
Jess Mitchell <jessicamitch42@gmail.com>
2018-02-28 02:32:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Write a function called theTruth, which returns the boolean true.
+function theTruth() {
+  return true;
+}
+// Write a function called sayHey, which returns the string 'Hey!'.
+function sayHey() {
+  return "Hey!";
+}
+// Write a function called sayHeyFriend, that takes one argument (the name of the friend). The function returns the string 'Hey <NAME>!' (e.g., 'Hey, Johnny!').
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`;
+}
+// Write a function called favoriteIceCream, which accepts a parameter and returns the string "I love <NAME_OF_ICECREAM>".
+function favoriteIceCream(ic) {
+  return `I love ${ic}`;
+}
+// Write a function called shouting, which accepts a string as a parameter and returns the string in all caps.
+function shouting(str) {
+  return str.toUpperCase();
+}
+// Write a function called roundDown, which accepts a number as parameter and returns the number rounded down the closest whole number.
+function roundDown(num) {
+  return Math.floor(num);
+}
