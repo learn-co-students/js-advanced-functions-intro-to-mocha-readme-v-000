0038084fb79b2dac655e3dcf9f6e79d36840cf59
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-01 03:44:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return "Hey!"
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`
+}
+
+function favoriteIceCream(choice) {
+    return `I love ${choice}`   
+}
+
+function shouting(word) {
+    return word.toUpperCase();
+    
+}
+
+function roundDown(num) {
+    return Math.floor(num)
+}
