42c44a6fd82d6ceb8c9d2edfc4806700ae7957b8
Morgan Fudge <mfudge92@gmail.com>
2018-04-01 23:50:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth() {
+  return true
+}
+function sayHey() {
+  return 'Hey!'
+}
+function sayHeyFriend(n) {
+ return `Hey, ${n}!`
+}
+function favoriteIceCream(i) {
+return `I love ${i}`
+}
+function shouting(s) {
+  return s.toUpperCase()
+}
+function roundDown(n) {
+  return parseInt(n)
+}
