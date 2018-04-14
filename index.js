2afd264ec877d0014c89e04f57f3953c4f5816d9
meq <fewell@gmail.com>
2018-04-14 16:51:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(friend) {
+  return 'Hey, ' + friend + "!"
+}
+
+function favoriteIceCream(flavor) {
+ return "I love " + flavor
+}
+
+function shouting(stringShout) {
+  return stringShout.toUpperCase()
+}
+
+function roundDown(numToRound){
+  return Math.floor(numToRound)
+}
