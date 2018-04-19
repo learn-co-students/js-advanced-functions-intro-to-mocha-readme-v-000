517a71c06088d97e7e5ac54fc5a0adb19d0c7205
mmmeyers <marsi286@gmail.com>
2018-04-19 16:05:27 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`
+}
+
+function shouting(word) {
+  return word.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
