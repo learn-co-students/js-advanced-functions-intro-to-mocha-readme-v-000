2b0811c613d81a4d5401b31e3778a969694bdab5
github username <github email address>
2018-05-13 09:30:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!";
+}
+
+const sayHeyFriend = name => `Hey, ${name}!`
+
+const favoriteIceCream = flavor => `I love ${flavor}`
+
+const shouting = string => string.toUpperCase()
+
+const roundDown = num => Math.floor(num)
