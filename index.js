135aacd854132169b49e93d25be423181c56c492
kostanzhoglo <jeffreywithers@gmail.com>
2018-05-16 16:46:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+  return `Hey, ${friendName}!`;
+}
+
+function favoriteIceCream(name) {
+  return `I love ${name}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
