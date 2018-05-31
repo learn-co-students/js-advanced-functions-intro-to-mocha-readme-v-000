315510bd328be46f6bc7bb87c646747c7849bc27
ronald vilorio <soccer23415@gmail.com>
2018-05-31 17:13:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+  return true
+}
+function sayHey(){
+  return "Hey!"
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(){
+  return 'I love mint chocolate chip'
+}
+function shouting(){
+  return 'I LOVE JAVASCRIPT SOOOOOO MUCH'
+}
+function roundDown(){
+  return 5
+}
