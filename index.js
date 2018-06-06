949646491b6f3086f4a15479a59ec933b4c09366
chaimoosh <chaimoosh@gmail.com>
2018-06-05 20:35:34 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(f) {
+  return `Hey, ${f}!`
+}
+
+function favoriteIceCream(flav) {
+  return `I love ${flav}`
+}
+
+function shouting(str) {
+  return str.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
