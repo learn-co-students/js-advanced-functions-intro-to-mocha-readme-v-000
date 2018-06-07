220ac2ee1efe0451e6e86da7f62f0392c913a7a1
Sam Marshall <samuel.r.marshall@gmail.com>
2018-06-06 20:21:20 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){return "Hey!"};
+
+function sayHeyFriend(friend){return `Hey, ${friend}!`};
+
+function favoriteIceCream(ic){return `I love ${ic}`}
+
+function shouting(st){return st.toUpperCase()}
+
+function roundDown(num){return Math.floor(num)}
