4c35098d89c2e3576fcd72613768ea8f55122a28
Eden <emesfun@outlook.com>
2018-06-11 09:05:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth(){
+    return true;
+}
+
+function sayHey() {
+    return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+    return `I love ${flavor}`;
+}
+
+function shouting(string) {
+    let caps = string.toUpperCase(); 
+    return caps;
+}
+
+function roundDown(number) {
+    let floor = Math.floor(number);
+    return floor;
+}
