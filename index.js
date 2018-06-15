bb1136c604719cc130d23ad17582228bfc2a440f
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-06-15 15:15:40 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth() {
+  return true;
+};
+function sayHey() {
+  return "Hey!";
+};
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+};
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+};
+function shouting(string) {
+  return string.toUpperCase();
+};
+function roundDown(integer) {
+  return Math.floor(integer);
+};
