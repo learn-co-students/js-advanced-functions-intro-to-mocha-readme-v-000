2d4eaeb5611fb109f80e26ea3cea79496c000cec
Joanne Lee <vernapii@gmail.com>
2018-06-27 11:42:20 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
+
+function favoriteIceCream(flavor) {
+  flavor = 'mint chocolate chip'
+  return `I love ${flavor}`
+}
