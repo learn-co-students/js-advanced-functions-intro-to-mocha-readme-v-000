25745e564daac6ddf258a95992dc024ced86aab4
badlychadly <chadmontoya21@yahoo.com>
2018-07-02 15:24:31 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+const theTruth = () => true
+
+const sayHey = () => "Hey!"
+
+const sayHeyFriend = (name) => `Hey, ${name}!`
+
+const favoriteIceCream = (flavor) => `I love ${flavor}`
+
+const shouting = (phrase) => phrase.toUpperCase();
+
+const roundDown = (num) => Math.floor(num) 
