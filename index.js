c67ae3c37e2df1e4b56d5212618b1b1ff9c11864
Michael Pope <popemray1986@gmail.com>
2018-07-03 03:59:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(friend) {
+  return "Hey," + " " + friend + "!"
+}
+
+function favoriteIceCream(flavor) {
+  return "I love" + " " + flavor
+}
+
+function shouting() {
+  const str = "i love javascript soooooo much"
+  return str.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
