6c482625f2fa2672125986b9371b5b475e2e05fe
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-29 14:41:10 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function favoriteIceCream(flavor){
+  // debugger
+ return  `I love ${flavor}`;
+}
+
+function sayHeyFriend(name){
+    return `Hey, ${name}!`;
+}
+
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return `Hey!`;
+}
+
+function shouting(str){
+  return `${str.toUpperCase()}`;
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
