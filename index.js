5b1d7492c143523a1642c349493019d8f05c827c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-03 19:16:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return 'Hey!'
+}
+
+function sayHeyFriend(friend){
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(icecream){
+  return `I love ${icecream}`
+}
+
+function shouting(string){
+  return string.toUppercase()
+}
+
+function roundDown(num){
+return floor(num)
+
+}
