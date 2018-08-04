fe89af56ca14ddb14d6e813d1c634aeea57b120c
Gingertonic <bethmschofield@gmail.com>
2018-08-04 21:06:16 +1000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+const theTruth = () => true;
+
+const sayHey = () => "Hey!"
+
+const sayHeyFriend = (name) => `Hey, ${name}!`
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase()
+}
+
+const roundDown = (x) => Math.floor(x)
