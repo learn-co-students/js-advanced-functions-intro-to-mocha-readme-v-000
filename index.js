d08039c37ce02e503be24ed3eaae8906387a399b
Vanessa Toro <vanetoro84@gmail.com>
2018-08-11 17:05:26 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+let theTruth = () => { return true}
+let sayHey = () => {return 'Hey!'}
+let sayHeyFriend = (name) => { return `Hey, ${name}!`}
+let favoriteIceCream = (flavor) => { return `I love ${flavor}`}
+let shouting = (shout) => {return shout.toUpperCase()}
+let roundDown = (num) => {return Math.floor(num)}
