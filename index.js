7b8e813817cc20ffef00f23a950dae5084f5ca7a
Rygel-XVI <micha232@umn.edu>
2018-08-28 17:44:27 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+const theTruth = () => true;
+
+const sayHey = () => 'Hey!';
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(param) {
+  return `I love ${param}`;
+}
+
+function shouting(str) {
+  return str.toUpperCase()
+}
+
+function roundDown(numb) {
+  return Math.floor(numb)
+}
