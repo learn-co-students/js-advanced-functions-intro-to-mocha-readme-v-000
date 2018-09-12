c87665d734dfa417a0aed9b9cb956e6df950686a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-12 11:10:25 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function theTruth() {
+	return true
+}
+ function sayHey() {
+	return "Hey!"
+}
+ function sayHeyFriend(friend) {
+	return `Hey, ${friend}!`
+}
+ function favoriteIceCream(flavor) {
+	return `I love ${flavor}`
+}
+ function shouting() {
+	const shout = 'i love javascript soooooo much'
+	return shout.toUpperCase();
+}
+ function roundDown(number) {
+	return Math.floor(number);
+} 
