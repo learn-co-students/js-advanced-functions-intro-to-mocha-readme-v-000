05642aad98f7845c56054fd4abee3aa06d84759d
CSavi <carrielane89@gmail.com>
2018-09-13 09:56:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return "Hey!";
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
 function favoriteIceCream(flavor) {
-    debugger
+   return `I love ${flavor}`;
+}
+
+function shouting(str) {
+    return str.toUpperCase();
+}
+
+function roundDown(num) {
+    return Math.floor(num);
 }
