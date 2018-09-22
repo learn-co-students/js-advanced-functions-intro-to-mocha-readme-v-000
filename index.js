2544eabc02969a7dcfd5dbff8717f8ebeff6113c
cjwood032 <cjwood032@gmail.com>
2018-09-22 15:10:18 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+    return true;
+}
+function sayHey(){
+    return 'Hey!';
+}
+function sayHeyFriend(friend){
+    return 'Hey, '+friend+'!';
+}
+function favoriteIceCream(flava){
+    return 'I love '+ flava;
+}
+function shouting(str) {
+    return str.toUpperCase();
+}
+    function roundDown(num){
+    return Math.floor(num);
+}
