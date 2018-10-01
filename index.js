deba51fd25dcf4b2bde610e3f7706993f87ef5b2
peterckim <peterckim96@gmail.com>
2018-10-01 13:58:06 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+  return `Hey, ${friendName}!`;
+}
+
+function favoriteIceCream(nameOfIceCream) {
+  return `I love ${nameOfIceCream}`;
+}
+
+function shouting(phrase) {
+  return `${phrase.toUpperCase()}`;
+}
+
+function roundDown(number) {
+  return `${Math.floor(number)}`;
+}
