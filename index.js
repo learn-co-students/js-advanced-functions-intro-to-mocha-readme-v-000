7162901e0b4bae3e6a55f5b99563f76249f354a1
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-15 18:02:09 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var theTruth =()=> true
+
+var sayHey =()=> "Hey!"
+
+var sayHeyFriend=(name)=> `Hey, ${name}!`
+
+var favoriteIceCream =(flavor) => `I love ${flavor}`
+
+var shouting=(str)=> str.toUpperCase()
+
+var roundDown=(num)=> Math.floor(num)
