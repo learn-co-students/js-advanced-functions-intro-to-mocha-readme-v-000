efafd65fd0b0eb776701cd34cb744227db1c5b50
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-10-17 16:55:11 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+   return "Hey!";
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(favorite) {
+    return `I love ${favorite}`;
+}
+
+function shouting(phrase) {
+    return phrase.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
