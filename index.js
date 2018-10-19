8b08d764a203680695918b1d5db017ce76012cf8
github username <github email address>
2018-10-18 18:11:17 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+let theTruth = () => ( true)
+
+let sayHey = () => ("Hey!")
+
+let sayHeyFriend = (name) => (`Hey, ${name}!`)
+
+let favoriteIceCream = (flavor) => (`I love ${flavor}`)
+
+let shouting = (phrase) => (phrase.toUpperCase())
+
+let roundDown = (num) => (Math.floor(num))
