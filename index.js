9833749d79d244775e62c92526bdbde92f5962f2
wali89 <wali.rahman89@yahoo.com>
2018-10-20 15:57:03 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var theTruth = () => {
+  return true
+
+}
+
+var sayHey = () => {
+  return "Hey!"
+}
+
+var sayHeyFriend = (name) => {
+  return "Hey, " + name + "!"
+}
+
+var favoriteIceCream = (flavor) => {
+  return "I love " + flavor
+
+}
+
+var shouting = (sentence) => {
+  return sentence.toUpperCase()
+}
+
+var roundDown = (num) => {
+  return Math.floor(num)
+}
