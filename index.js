48edf001bef7639400d2dbb2a1711a8fe3b6d61e
roitblatari <roitblatari@gmail.com>
2018-11-15 12:27:25 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+const theTruth = () => {
+  return true || false;
+}
+
+const sayHey = () => {
+  return "Hey!";
+}
+
+const sayHeyFriend = (name) => {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  // debugger;
+  return "I love mint chocolate chip";
+}
+
+const shouting = () => {
+  return "I LOVE JAVASCRIPT SOOOOOO MUCH";
+}
+
+const roundDown = (n) => {
+  return Math.floor(n) ;
+}
