c1dd16956e0264d6b73a331df43f5c968ee9e9b2
Ethel C <cofresi.ethel@gmail.com>
2018-11-20 20:33:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return 'Hey!';
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(name){
+  return `I love ${name}`;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
