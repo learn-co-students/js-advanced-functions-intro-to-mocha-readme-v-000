2386a23f75a64c74935baae85c859550857309dd
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 23:47:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theTruth() {
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor){
+return `I love ${flavor}`
+}
+
+function shouting(someString){
+  return someString.toUpperCase()
+}
+
+
+function roundDown(someNum){
+  if (someNum)
+  return Math.floor(someNum)
+
+}
