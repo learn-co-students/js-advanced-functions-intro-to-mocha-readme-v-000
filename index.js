b829c066ed6009888ff55de0bd7dd19b6a64d9d9
anikonchuk <anikonch@gmail.com>
2018-11-28 13:07:15 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(inputString) {
+  return inputString.toUpperCase();
+}
