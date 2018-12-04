5117e0fc650a27b917ca7d092fd319f686f2d42c
Jayon Kim <jpkim921@gmail.com>
2018-12-03 23:14:21 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+// function theTruth() {
+//   return true;
+// }
+
+let theTruth = () => true;
+let sayHey = () => 'Hey!'
+let sayHeyFriend = (name) => `Hey, ${name}!`
+let shouting = (string) => string.toUpperCase();
+let roundDown = (num) => Math.floor(num);
