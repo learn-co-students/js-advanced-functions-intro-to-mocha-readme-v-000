2cd5576f7786cfe199c254c472a68291e040f007
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-01-03 17:24:59 -0500

    Done.

--- test/root.js
+++ test/root.js
@@ -1,24 +1,37 @@
-global.expect = require('expect');
+// global.expect = require('expect');
 
-const jsdom = require('jsdom');
-const path = require('path');
+// const jsdom = require('jsdom');
+// const path = require('path');
 
-before(function(done) {
-  const src = path.resolve(__dirname, '..', 'index.js');
-  const babelResult = require('babel-core').transformFileSync(src, {
-    presets: ['env']
-  });
-  const html = path.resolve(__dirname, '..', 'index.html');
+// before(function(done) {
+//   const src = path.resolve(__dirname, '..', 'index.js');
+//   const babelResult = require('babel-core').transformFileSync(src, {
+//     presets: ['env']
+//   });
+//   const html = path.resolve(__dirname, '..', 'index.html');
 
-  jsdom.env(html, [], { src: babelResult.code }, (err, window) => {
-    if (err) {
-      return done(err);
-    }
+//   jsdom.env(html, [], { src: babelResult.code }, (err, window) => {
+//     if (err) {
+//       return done(err);
+//     }
 
-    Object.keys(window).forEach(key => {
-      global[key] = window[key];
-    });
+//     Object.keys(window).forEach(key => {
+//       global[key] = window[key];
+//     });
 
-    return done();
-  });
-});
+//     return done();
+//   });
+// });
+global.expect = require('expect')
+
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
+const src = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
+
+jsdom({
+  html,
+  src
+})
