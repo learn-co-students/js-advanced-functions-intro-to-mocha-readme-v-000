cf0053d5c9a5a1e18f1983dbf348857b9bb4444d
micahshute <micah.shute@gmail.com>
2019-01-07 14:21:22 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+const theTruth = () => true;
+const sayHey = () => "Hey!";
+const sayHeyFriend = (name) => `Hey, ${name}!`;
+const favoriteIceCream = (ic) => `I love ${ic}`;
+const shouting = (phrase) => phrase.toUpperCase();
+const roundDown = (num) => Math.floor(num);
+
+
