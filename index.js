a484f97fe015bf2a655b4b13f404b9ef96a7e063
Madeline-Stark <madeline.stark1@gmail.com>
2019-01-25 13:07:51 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const theTruth = function() {
+  return true;
+}
+
+const sayHey = function() {
+  return 'Hey!';
+}
+
+const sayHeyFriend = function(name) {
+  return `Hey, ${name}!`;
+}
+
+const favoriteIceCream = function(flavor) {
+  return `I love ${flavor}`;
+}
+
+const shouting = function(phrase) {
+  return phrase.toUpperCase();
+}
+
+const roundDown = function(number) {
+  return Math.floor(number);
+}
