f4ea45a4df77ebb0448341c4633962350111890f
lauradarlak <lauradarlak@gmail.com>
2019-01-31 16:26:22 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(str) {
+  return str.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
