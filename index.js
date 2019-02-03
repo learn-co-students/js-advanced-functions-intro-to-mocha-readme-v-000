acdcfe86a8b49e6f9c1debe4b856205712813906
David McGrann <dpmcgrann@gmail.com>
2019-02-03 19:17:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(friend) {
+  return "Hey," + " " + `${friend}` + "!";
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(str) {
+  return `${str.toUpperCase()}`;
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
