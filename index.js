a73f01957b1911c332a1ad1f97f317bd2ac49dc9
atxrenegade <harleighabel@gmail.com>
2019-02-03 22:01:19 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const theTruth = () => {
+	return true;
+}
+
+const sayHey = () => {
+	return 'Hey!';
+}
+
+const sayHeyFriend = (friendName) => {
+	return `Hey, ${friendName}!`;
+}
+
+const favoriteIceCream = (icecreamFlavor) => {
+	return `I love ${icecreamFlavor}`
+}
+
+const shouting = (string) => {
+	return string.toUpperCase()
+}
+
+const roundDown = (num) => {
+	return Math.floor(num)
+}
