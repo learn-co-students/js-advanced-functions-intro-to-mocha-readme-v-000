ae581be06efd90cc4523e1aae3e607ea7c2cca2e
ConorHamilton19 <conhamil19@gmail.com>
2019-02-11 13:09:21 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey () {
+  return "Hey!"
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(name) {
+  return `I love ${name}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.floor(number)
+}
