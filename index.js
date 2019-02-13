c001ec3b0983cbe27685d330b80acffc3f64d1e9
github username <github email address>
2019-02-13 17:11:40 -0500

    practice reading tests

---
+++ index.js
@@ -0,0 +1,25 @@
+function favoriteIceCream(flavor) {
+  var favoriteFlavor = `I love ${flavor}`;
+  return favoriteFlavor;
+}
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  var greeting = `Hey, ${name}!`;
+  return greeting;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
