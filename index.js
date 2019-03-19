e00e09fdb351079126fe373e69c8d5a3268873a4
Saad Ahmed <dropheaven@live.com>
2019-03-19 06:44:59 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+const theTruth = () => true;
+const sayHey = () => "Hey!";
+const sayHeyFriend = name => `Hey, ${name}!`;
+const favoriteIceCream = icecream => `I love ${icecream}`;
+const shouting = string => string.toUpperCase();
+const roundDown = float => Math.floor(float);
