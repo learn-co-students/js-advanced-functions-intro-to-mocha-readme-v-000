f87e277516a73c3d147d344d05ad40524ae244d4
Mohammed Noori <kmanoorib@gmail.com>
2019-04-01 04:49:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function theTruth() {return true;}
+
+function favoriteIceCream(flavor) {return "I love " + flavor;}
+    
+function sayHey() {return 'Hey!'}
+
+function sayHeyFriend(name) {return 'Hey, ' + name + '!'}
+
+function shouting(word) {return word.toUpperCase()}
+
+function roundDown(num) {return Math.floor(num)}
