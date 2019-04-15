c7a20b35f9591aa237e487832ab14f8453c84652
Joeycho <joeyadamcho@gmail.com>
2019-04-15 16:24:57 +0900

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase()
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function theTruth() {
+  return true
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
