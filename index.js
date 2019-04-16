f90caa264dd3b5136f4ab7191dd2f14e241ef089
Elizabeth <efkarst@outlook.com>
2019-04-16 11:14:56 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey () {
+  return 'Hey!'
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(ice_cream) {
+  return `I love ${ice_cream}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(num) {
+  return num >> 0
+}
