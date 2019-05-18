0209811401fcee421136eda8c524ac206f65be7d
corytveten <corytveten@gmail.com>
2019-05-18 07:02:21 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream() {
+    return 'I love mint chocolate chip';
+  }
+
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return "Hey!";
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function shouting(string) {
+    return string.toUpperCase();
+}
+
+function roundDown(n) {
+    return Math.floor(n);
+}
