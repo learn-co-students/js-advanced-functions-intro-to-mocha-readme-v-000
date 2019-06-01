c330ac6774a46348644de5b205d05243a3b5a5af
RonSala <ron.sala@gmail.com>
2019-05-31 22:14:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function favoriteIceCream(flavor) {
-  // debugger;
-}
-
 function theTruth() {
   return true;
 }
 
 function sayHey() {
-  return 'Hey!';
+  return `Hey!`;
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(name) {
+  return `I love ${name}`;
+}
+
+function shouting(str) {
+  return str.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
 }
