79480a895c1bd6a1ff2195a2940e89bca0ba502b
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-08 13:30:14 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return "Hey!";
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(ice_cream_name) {
+    return `I love ${ice_cream_name}`;
+}
+
+function shouting(str) {
+    return str.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
