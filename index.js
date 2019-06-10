8e7d742cfcab8080e89a1cd8da4fc65cef5b4d46
MDAM182 <mdam2345@gmail.com>
2019-06-10 19:03:21 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth(){
+  const boolean = true
+  return boolean
+}
+
+function sayHey(){
+  const string = "Hey!"
+  return string
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`
+}
+
+function shouting(string){
+      return string.toUpperCase()
+}
+
+function roundDown(number){
+  return Math.floor(number)
+}
