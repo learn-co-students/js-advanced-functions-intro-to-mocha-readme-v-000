7f68f52a1f4d3bd29cb5dd2d5e3f74f126f31794
catherinism <catcreates13@gmail.com>
2019-06-17 06:11:33 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return "Hey!";
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(ice_cream) {
+    return `I love ${ice_cream}`;
+}
+
+function shouting(string) {
+    return string.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
