0d484d2506a77ecc9df0071888bff6813f1b0df6
github username <github email address>
2019-07-21 10:00:48 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+theTruth = function(){
+  return true;
+}
+
+sayHey = function(){
+  return 'Hey!'
+}
+
+sayHeyFriend = function(name){
+  return `Hey, ${name}!`
+}
+
+favoriteIceCream = function(flavor){
+  return (`I love ${flavor}`)
+}
+
+shouting = function(shout){
+  return 'i love javascript soooooo much'.toUpperCase();
+}
+
+roundDown = function(number){
+  return Math.floor(number);
+}
